e0d7f6952cd8057be1c3d7e83355155fe8b7ad64
karl newton <newkarl@gmail.com>
2019-08-12 21:12:12 -0500

    bugsfixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :courses
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
