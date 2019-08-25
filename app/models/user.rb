1335f7af0e722440e6acb20d88c1e71d7b79bc66
karl newton <newkarl@gmail.com>
2019-08-25 12:27:10 -0500

    flixter27

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
+
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
+
 
 end
