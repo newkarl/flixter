9e55d59c9136af46ca811cf95b2f12803c1e04ce
karl newton <newkarl@gmail.com>
2019-08-25 11:47:06 -0500

    flixter25

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,16 +1,12 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  storage :aws
   # storage :file
   # storage :fog
-  if Rails.env.production?
-    storage :aws
-  else
-    storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -28,14 +24,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fill: [800, 350]
+  #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  # process resize_to_fit: [50, 50]
+  #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "private"
+  if Rails.env.production?
+    config.storage    = :aws
+    config.aws_bucket = ENV["AWS_BUCKET"]
+    config.aws_acl    = "public-read"
 
-  config.aws_credentials = {
-      access_key_id:     ENV["AWS_ACCESS_KEY"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      region:            ENV["AWS_REGION"]
-  }
+    config.aws_credentials = {
+        access_key_id:     ENV["AWS_ACCESS_KEY"],
+        secret_access_key: ENV["AWS_SECRET_KEY"],
+        region:            ENV["AWS_REGION"]
+    }
+  end
 end
