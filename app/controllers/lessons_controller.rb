2a1b8967affa4b9f1810b9d74663827ce44ecec0
karl newton <newkarl@gmail.com>
2019-08-29 20:03:53 -0500

    flixter39addedsomestuff

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,28 +1,30 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :student_enrolled
 
-  def new
-    @course.user = course.new
-  end
+  # def new
+  #   @course.user = course.new
+  # end
 
-  def create
-    @course.user = current_user.courses.sections(course_params)
-    if @course.user.valid?
-      redirect_to student_course_path(@course)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @course.user = current_user.courses.sections(course_params)
+  #   if @course.user.valid?
+  #     redirect_to student_course_path(@course)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
   def show
   end
 
   private
 
-  def require_authorized_for_current_user
-    if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+  def student_enrolled
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course),alert:"must be enrolled first"
     end
+  end
 
   helper_method :current_lesson
   def current_lesson
